67560239d8367c92c3fc964615c5f45f6f74d728
wfeng19 <willf182@gmail.com>
2017-02-09 21:52:40 -0500

    Add files via upload

---
+++ The_Clairvoyent.java
@@ -0,0 +1,271 @@
+package the_clairvoyent;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class The_Clairvoyent {
+
+	//Yes or No
+	static String[] choices = new String[]
+			{"Yes", "No"};
+	//Array with possible inputted names
+	static String[] names = new String[]
+			{"Caitlin"};
+	//Introduction dialogue
+	static String[] dialogue = new String[]
+			{"I am the Clairvoyent. I have the ability to see the future based on various circumstances, such as the time of day,",
+			"weather, and more. Thus, I will be asking you a series of 10 questions, some of which will require a yes or no answer.",
+			"These questions will help me get a better picture of the future, which I will then illustrate for you. However, my visions only",
+			"come in bits and pieces, so that will be the best I can give you. Please answer only using lowercase letters and be truthful.",
+			"If none of the answers are correct, please select the one that best fits.",
+			"Is this understood? [Yes/No]"
+			};
+	//Arrays for feelings feelings
+	static String[] feelings_a = new String[]
+			{"FAN-FREAKING-TASTIC",
+			"Pretty good",
+			"Eh, I'm ok",
+			"Pretty bad",
+			"I want to kill myself"
+			};
+	static boolean[] feelings_b = new boolean[]
+			{false,
+			false,
+			false,
+			false,
+			false					
+			};
+	//Arrays for weather		
+	static String[] weather_a = new String[]
+			{"Sunny",
+			"Party cloudy",
+			"Cloudy",
+			"Rainy",
+			"Snowy"		
+			};
+	static String[] weather_r = new String[]
+			{"I wish I could feel the sunshine... Thanks for making me feel bad.",
+			"Well at least it's not completely cloudy!",
+			"Well at least it's not raining!",
+			"Ew, I can't touch the rain, or I'm gonna get fried! I really hope you don't bring outside.",
+			"Really? That's AWESOME!",
+			"Can you please retype your answer?"
+			};
+	static boolean[] weather_b = new boolean[]
+			{false, false, false, false, false};
+	
+	
+	public static void main(String[] args) {
+		
+		boolean validInput = false;
+		boolean yes;
+		boolean Caitlin;
+		String miscAnswers;
+		String answer;
+		
+		//Introduction with name
+		System.out.println("Good afternoon. May I inquire as to what your name is?");
+		Scanner input = new Scanner(System.in);
+		String name = input.nextLine();
+		System.out.println("Hi " + name + ", it is pleasure to meet you!");
+		
+		//"How are you today?"- First real question
+		System.out.println("How are you today? [ FAN-FREAKING-TASTIC / Pretty good / Eh, I'm ok / Pretty bad / I want to kill myself ]");
+		String feeling = input.nextLine();
+		validInput = false;
+		while (!validInput){
+			if (feelings_b[0] = (feeling).equalsIgnoreCase(feelings_a[0])){
+				System.out.println("Well that is simply AMAZING to hear. I'm glad you're having such an awesome day!");
+				validInput = true;
+			}
+			else if (feelings_b[1] = (feeling).equalsIgnoreCase(feelings_a[1])){
+				System.out.println("That's good.");
+				validInput = true;
+			}
+			else if (feelings_b[2] = (feeling).equalsIgnoreCase(feelings_a[2])){
+				System.out.println("Ah, one of those days, huh? Nothing too good or too bad? I gotchu.");
+				validInput = true;
+			}
+			else if (feelings_b[3] = (feeling).equalsIgnoreCase(feelings_a[3])){
+				System.out.println("Aw, I'm sorry to hear that. I hope that I'll be able to brighten up your day very soon!");
+				validInput = true;
+			}
+			else if (feelings_b[4] = (feeling).equalsIgnoreCase(feelings_a[4])){
+				System.out.println("No, please don't say that! There are still so many things in life to care about, and there are");
+				System.out.println("still so many things you haven't had the chance to do yet. Even if you don't care about your life,");
+				System.out.println("just imagine how sad your friends and family would feel if you committed suicide. And if you honestly");
+				System.out.println("think that no one would miss you if you suddenly disappeared, that's simply not true. Each life touches");
+				System.out.println("the lives of thousands of others. I thus beg that you stay here until you at least finish running through");
+				System.out.println("my program. If you still want to take your life after this, then so be it. But at least allow me to try");
+				System.out.println("and brighten up your day.");
+				validInput = true;
+			}
+			else{
+				System.out.println("What did you just say?");
+				validInput = false;
+				feeling = input.nextLine();
+			}
+		}
+		validInput = false;
+		
+		//Explanation of purpose
+		System.out.println("I am unsure about how much my creator has told you about me, so please allow me to elaborate.");
+		printArray(dialogue);
+		while (!isCorrectAnswer(input.nextLine()));
+		System.out.println("Let's start with some deeply personal and creepy questions.");
+        validInput = false;
+       
+        //Personal Question 1- Age
+        while (!validInput){
+        	System.out.println("How old are you? [Enter an integer in years]");
+        	try {
+        		int age = input.nextInt();
+        		input.nextLine();
+        		System.out.println("Yeah, I am only about a week old, so you're definitely older than me.");
+        		validInput = true;
+        	}catch (InputMismatchException e){
+        		System.out.println("What the heck does that mean?");
+        		input.nextLine();
+        	}
+        }
+        validInput = false;
+        
+        //Personal Question 2- Pets
+        while (!validInput){
+        	System.out.println("Do you have any pets? [Yes/No]");
+        	answer = input.nextLine();
+        	yes = (answer).equalsIgnoreCase(choices[0]);
+        	if (yes){
+        		System.out.println("How many? [Enter an integer]");
+        		try {
+            		int pets = input.nextInt();
+            		input.nextLine();
+            		System.out.println("Only? I have around 1729 cats, but I guess " + pets + " works, too.");
+            		validInput = true;
+            	}catch (InputMismatchException e){
+            		System.out.println("What? I'll ask again.");
+            		validInput = false;
+            		input.nextLine();
+            	}
+        	}
+        	else if (!yes){
+        		System.out.println("How can you not have any pets? Pets are great! [Explain your answer]");
+        		miscAnswers = input.nextLine();
+        		System.out.println("Oh, ok. Whatever, your loss.");
+        		validInput = true;
+        	}
+        	else {
+        		System.out.println("I have no idea what you mean, but I will continue, I guess.");
+        	}
+        }
+        validInput = false;
+        
+        //Personal Question 3- Make out
+        System.out.println("Out of the people in your school, who do you want to make out with the most?");
+        try {
+        	Thread.sleep(5000);
+        	System.out.println("");
+        }
+        catch (InterruptedException e) {
+        	e.printStackTrace();
+        }
+        while (!validInput){
+        	System.out.println("Haha, I'm just joking. You don't have to answer that!");
+        	//Tests for names to output different stuff
+        	if (Caitlin = (name).equalsIgnoreCase(names[0])){
+        		System.out.println("I'm sure your answer would be Matt Kraft, though.");
+                System.out.println("But ANYWAYS...");
+        		validInput = true;
+        	}
+        	else{
+        		validInput = true;
+        	}
+        }
+        validInput = false;
+        
+        //Introduction to Actual Questions
+        try {
+        	Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+        	e.printStackTrace();
+        }
+        System.out.println("Can I be honest with you? You're kinda boring. You're boring me. I think I'm just going to move onto the actual");
+        System.out.println("questions now. Yeah, you heard me correctly. I was just messing around before. But since you're so boring");
+        System.out.println("I guess I have to actually do my thing now and read the future and blah blah blah. Ugh. Here we go.");
+        try {
+        	Thread.sleep(3000);
+            System.out.println("");
+        }
+        catch (InterruptedException e) {
+        	e.printStackTrace();
+        }
+        //Question 1- Weather
+        System.out.println("Question 1:");
+        System.out.println("What is the weather like today? [ Sunny / Partly Cloudy / Cloudy / Windy / Stormy ]");
+        while (!mc_questions(input.nextLine(), weather_a, weather_r, weather_b));
+        //Question 2- Temperature
+        System.out.println("Question 2:");
+	}
+	
+	
+	
+	//Method to make sure user inputs "Yes", and will keep repeating otherwise
+	static boolean isCorrectAnswer(String userAnswer) {
+	    boolean rightAnswer = (userAnswer).equalsIgnoreCase(choices[0]);
+	    if (rightAnswer){
+	    	System.out.println("Thank you, and let the questions begin!");
+	    }
+	    else if (!rightAnswer){
+	    	System.out.println("Allow me to repeat myself then.");
+	    	printArray(dialogue);
+	    }
+	    return rightAnswer;
+	}
+	
+	//Method for the multiple choices questions
+	//Note: It would be cool to eventually keep score of what answer the user actually picks with an integer, and each
+	//if and else statement would add to the score. That way, the picture that's outputted would actually change depending
+	//on what answers you pick. However, I still must test to see if the score is kept or if it can be outputted from the method.
+	static boolean mc_questions(String userAnswer, String[] answers, String[] responses, boolean[] booleans) {
+	    boolean moveOn = false;
+	    if (booleans[0] = (userAnswer).equalsIgnoreCase(answers[0])){
+			System.out.println(responses[0]);
+			System.out.println("");
+			moveOn = true;
+		}
+		else if (booleans[1] = (userAnswer).equalsIgnoreCase(answers[1])){
+			System.out.println(responses[1]);
+			System.out.println("");
+			moveOn = true;
+		}
+		else if (booleans[2] = (userAnswer).equalsIgnoreCase(answers[2])){
+			System.out.println(responses[2]);
+			System.out.println("");
+			moveOn = true;
+		}
+		else if (booleans[3] = (userAnswer).equalsIgnoreCase(answers[3])){
+			System.out.println(responses[3]);
+			System.out.println("");
+			moveOn = true;
+		}
+		else if (booleans[4] = (userAnswer).equalsIgnoreCase(answers[4])){
+			System.out.println(responses[4]);
+			System.out.println("");
+			moveOn = true;
+		}
+		else{
+			System.out.println(responses[5]);
+		}
+	    return moveOn;
+	}
+	
+	//Method for outputting arrays
+	public static void printArray(String[] anArray) {
+		for (int i = 0; i < anArray.length; i++) {
+			System.out.println(anArray[i]);
+		}
+	}
+	
+}
